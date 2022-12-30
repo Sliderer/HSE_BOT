b45bc74290a5fa8f97582ff029ba65d32a6a206f
Sliderer <slidedru8gs@mail.ru>
2022-12-30 14:45:17 +0300

    create bot connection

--- HSE_BOT/main.py
+++ HSE_BOT/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from config import bot_token
+from aiogram import Bot, Dispatcher
+from aiogram.utils import executor
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = Bot(bot_token)
+dispatcher = Dispatcher(bot)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    executor.start_polling(dispatcher)

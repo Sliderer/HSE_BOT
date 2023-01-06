152785253b1708f27591d3b76b35338b6f4b2028
Sliderer <slidedru8gs@mail.ru>
2023-01-06 21:08:49 +0300

    add ruz pasring

--- HSE_BOT/handlers/users/ruz_parsing.py
+++ HSE_BOT/handlers/users/ruz_parsing.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher.storage import FSMContext
 
 from config import dispatcher
 from states import Parsing
+import threading
 from ruz_parser import Parser
 
 @dispatcher.message_handler(commands='parse_ruz')
@@ -13,8 +14,12 @@ async def start_parsing_ruz(message: types.Message):
 
 @dispatcher.message_handler(state=Parsing.writing_user_name)
 async def get_full_name(message: types.Message, state: FSMContext):
-    parser = Parser()
     name = message.text
+    thread = threading.Thread(target=get_shedule, kwargs={'name': name})
+    thread.start()
+    await state.reset_state()
+
+def get_shedule(name):
+    parser = Parser()
     answer = parser.get_lessons(name)
     print(answer)
-    await state.reset_state()

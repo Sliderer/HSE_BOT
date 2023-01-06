4969a117b0588820d3ddf29730c5120590bc14d0
Sliderer <slidedru8gs@mail.ru>
2023-01-06 20:25:51 +0300

    solve

--- HSE_BOT/handlers/users/ruz_parsing.py
+++ HSE_BOT/handlers/users/ruz_parsing.py
@@ -5,6 +5,7 @@ from config import dispatcher
 from states import Parsing
 from ruz_parser import Parser
 
+
 @dispatcher.message_handler(commands='parse_ruz')
 async def start_parsing_ruz(message: types.Message):
     await message.answer('Enter your full name/help')
@@ -13,7 +14,5 @@ async def start_parsing_ruz(message: types.Message):
 
 @dispatcher.message_handler(state=Parsing.writing_user_name)
 async def get_full_name(message: types.Message, state: FSMContext):
-    parser = Parser()
-    answer = await parser.parse()
-    await message.answer(answer)
+    await message.answer('')
     await state.reset_state()

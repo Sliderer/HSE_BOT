523dc0322db5ab129cf02c565db53d231ad6711c
Sliderer <slidedru8gs@mail.ru>
2023-01-07 16:49:20 +0300

    add reply markup

--- HSE_BOT/handlers/users/message_handlers.py
+++ HSE_BOT/handlers/users/message_handlers.py
@@ -1,4 +1,4 @@
-from config import dispatcher, database
+from config import dispatcher, database, commands_names
 from aiogram import types
 from filters import IsChatPrivate
 from models import User
@@ -6,13 +6,19 @@ from models import User
 
 @dispatcher.message_handler(IsChatPrivate(), commands=['start'])
 async def start_bot(message: types.Message):
-    await message.answer(f'Hello {message.from_user.first_name}. I am a bot for HSE students!')
     user_id = message.from_user.id
     first_name = message.from_user.first_name
     second_name = message.from_user.last_name
 
     user = User(user_id, first_name, second_name)
     database.add_user(user)
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for name in commands_names:
+        button = types.KeyboardButton(name)
+        markup.add(button)
+
+    await message.answer(f'Hello {message.from_user.first_name}. I am a bot for HSE students!', reply_markup=markup)
 
 
 @dispatcher.message_handler(commands=['help'])

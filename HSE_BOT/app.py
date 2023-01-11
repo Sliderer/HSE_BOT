540c12e32c800a0a6ee17db941dc6e6ea8ccaa6e
Sliderer <a6271547@mail.ru>
2023-01-11 19:00:55 +0300

    upd

--- HSE_BOT/app.py
+++ HSE_BOT/app.py
@@ -1,10 +1,30 @@
 from aiogram.utils import executor
 from config import dispatcher
 import filters, handlers, errors
+from multiprocessing import Process
+import asyncio
+
+
+def start_bot():
+    print('start bot')
+    executor.start_polling(dispatcher)
+
+
+def start_schedule():
+    print('schedule')
+    while True:
+        pass
+
 
 if __name__ == '__main__':
-    print('Start bot')
-    try:
-        executor.start_polling(dispatcher)
-    except:
-        print('Error with starting bot')
+    schedule_thread = Process(target=start_schedule)
+    bot_thread = Process(target=start_bot)
+
+    schedule_thread.start()
+    bot_thread.start()
+
+    schedule_thread.join()
+    bot_thread.join()
+
+
+

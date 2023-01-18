0faa762b0d8d76d4152791e7f695900d191522a6
Sliderer <slidedru8gs@mail.ru>
2023-01-18 13:13:35 +0300

    upd: update state functions

--- HSE_BOT/handlers/users/username_saving.py
+++ HSE_BOT/handlers/users/username_saving.py
@@ -1,7 +1,7 @@
 from config import dispatcher, database
 from aiogram import types
 from states import SavingUsername
-
+from aiogram.dispatcher.storage import FSMContext
 
 @dispatcher.message_handler(commands=['save_name'])
 async def username_saving(message: types.Message):
@@ -10,11 +10,16 @@ async def username_saving(message: types.Message):
 
 
 @dispatcher.message_handler(state=SavingUsername.entering_name)
-async def get_username(message: types.Message):
+async def get_username(message: types.Message, state: FSMContext):
     username = message.text
     user_id = message.from_user.id
     #checking if student existing
+    if not 1:
+        await message.answer('Student not found. Please enter your full name')
+        return
+
     database.update_username(user_id, username)
     await message.answer('Saved!')
+    await state.reset_state()
 
 

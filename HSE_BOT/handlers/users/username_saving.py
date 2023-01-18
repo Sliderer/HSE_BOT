57307992ad3e7725f06d132d1b8c79a719025eaa
Sliderer <slidedru8gs@mail.ru>
2023-01-18 13:35:14 +0300

    add username saving

---
+++ HSE_BOT/handlers/users/username_saving.py
@@ -0,0 +1,25 @@
+from config import dispatcher, database
+from aiogram import types
+from states import SavingUsername
+from aiogram.dispatcher.storage import FSMContext
+
+@dispatcher.message_handler(commands=['save_name'])
+async def username_saving(message: types.Message):
+    await message.answer('Please send your full name')
+    await SavingUsername.first()
+
+
+@dispatcher.message_handler(state=SavingUsername.entering_name)
+async def get_username(message: types.Message, state: FSMContext):
+    username = message.text
+    user_id = message.from_user.id
+    #checking if student existing
+    if not 1:
+        await message.answer('Student not found. Please enter your full name')
+        return
+
+    database.update_username(user_id, username)
+    await message.answer('Saved!')
+    await state.reset_state()
+
+
